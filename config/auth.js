d69aa02611de79c536aed6129dab02778ffff831
Hagil Baik <hagilbaik@gmail.com>
2017-11-07 11:59:32 +0800

    Mirrors should reflect a little before throwing back images. 	-- Jean Cocteau

---
+++ config/auth.js
@@ -0,0 +1,21 @@
+module.exports = {
+    
+        'facebookAuth' : {
+            'clientID'      : '1995258940754978', // your App ID
+            'clientSecret'  : 'e2416cc847455197d89b57ae6dd94685', // your App Secret
+            'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        },
+    
+        'twitterAuth' : {
+            'consumerKey'       : 'your-consumer-key-here',
+            'consumerSecret'    : 'your-client-secret-here',
+            'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+        },
+    
+        'googleAuth' : {
+            'clientID'      : 'your-secret-clientID-here',
+            'clientSecret'  : 'your-client-secret-here',
+            'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        }
+    
+    };
