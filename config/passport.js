488dfc99cc5d382bdcbe4aa67c6ccee60e38aff4
Hagil Baik <hagilbaik@gmail.com>
2017-11-07 16:21:25 +0800

    Barth's Distinction: 	There are two types of people: those who divide people into two 	types, and those who don't.

--- config/passport.js
+++ config/passport.js
@@ -118,8 +118,8 @@ module.exports = function (passport) {
             // pull in our app id and secret from our auth.js file
             clientID: configAuth.facebookAuth.clientID,
             clientSecret: configAuth.facebookAuth.clientSecret,
-            callbackURL: configAuth.facebookAuth.callbackURL
-
+            callbackURL: configAuth.facebookAuth.callbackURL,
+            profileFields: configAuth.facebookAuth.profileFields
         },
 
         // facebook will send back the token and profile
