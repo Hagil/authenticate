39f7a860f61bad5c14608b2eb6b6b083dfefc404
Hagil Baik <hagilbaik@gmail.com>
2017-11-08 09:56:24 +0800

    The time for action is past!  Now is the time for senseless bickering.

--- config/passport.js
+++ config/passport.js
@@ -29,6 +29,32 @@ module.exports = function (passport) {
         });
     });
 
+    passport.use('local-login', new LocalStrategy({
+            // by default, local strategy uses username and password, we will override with email
+            usernameField: 'email',
+            passwordField: 'password',
+            passReqToCallback: true // allows us to pass back the entire request to the callback
+        },
+        function (req, email, password, done) { // callback with email and password from our form
+            // find a user whose email is the same as the forms email
+            // we are checking to see if the user trying to login already exists
+            User.findOne({
+                'local.email': email
+            }, function (err, user) {
+                // if there are any errors, return the error before anything else
+                if (err)
+                    return done(err);
+                // if no user is found, return the message
+                if (!user)
+                    return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+                // if the user is found but the password is wrong
+                if (!user.validPassword(password))
+                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+                // all is well, return successful user
+                return done(null, user);
+            });
+        }));
+
     // =========================================================================
     // LOCAL SIGNUP ============================================================
     // =========================================================================
@@ -42,11 +68,9 @@ module.exports = function (passport) {
             passReqToCallback: true // allows us to pass back the entire request to the callback
         },
         function (req, email, password, done) {
-
             // asynchronous
             // User.findOne wont fire unless data is sent back
             process.nextTick(function () {
-
                 // find a user whose email is the same as the forms email
                 // we are checking to see if the user trying to login already exists
                 User.findOne({
@@ -55,20 +79,16 @@ module.exports = function (passport) {
                     // if there are any errors, return the error
                     if (err)
                         return done(err);
-
                     // check to see if theres already a user with that email
                     if (user) {
                         return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
                     } else {
-
                         // if there is no user with that email
                         // create the user
                         var newUser = new User();
-
                         // set the user's local credentials
                         newUser.local.email = email;
                         newUser.local.password = newUser.generateHash(password);
-
                         // save the user
                         newUser.save(function (err) {
                             if (err)
@@ -76,45 +96,11 @@ module.exports = function (passport) {
                             return done(null, newUser);
                         });
                     }
-
                 });
-
-            });
-
-        }));
-
-    passport.use('local-login', new LocalStrategy({
-            // by default, local strategy uses username and password, we will override with email
-            usernameField: 'email',
-            passwordField: 'password',
-            passReqToCallback: true // allows us to pass back the entire request to the callback
-        },
-        function (req, email, password, done) { // callback with email and password from our form
-
-            // find a user whose email is the same as the forms email
-            // we are checking to see if the user trying to login already exists
-            User.findOne({
-                'local.email': email
-            }, function (err, user) {
-                // if there are any errors, return the error before anything else
-                if (err)
-                    return done(err);
-
-                // if no user is found, return the message
-                if (!user)
-                    return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
-
-                // if the user is found but the password is wrong
-                if (!user.validPassword(password))
-                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
-
-                // all is well, return successful user
-                return done(null, user);
             });
         }));
 
     passport.use(new FacebookStrategy({
-
             // pull in our app id and secret from our auth.js file
             clientID: configAuth.facebookAuth.clientID,
             clientSecret: configAuth.facebookAuth.clientSecret,
@@ -122,20 +108,15 @@ module.exports = function (passport) {
             profileFields: configAuth.facebookAuth.profileFields,
             passReqToCallback: true
         },
-
         // facebook will send back the token and profile
-        function (token, refreshToken, profile, done) {
-
+        function (req, token, refreshToken, profile, done) {
             // asynchronous
             process.nextTick(function () {
-
                 if (!req.user) {
-
                     // find the user in the database based on their facebook id
                     User.findOne({
                         'facebook.id': profile.id
                     }, function (err, user) {
-
                         // if there is an error, stop everything and return that
                         // ie an error connecting to the database
                         if (err)
@@ -143,6 +124,17 @@ module.exports = function (passport) {
 
                         // if the user is found, then log them in
                         if (user) {
+                            if (!user.facebook.token) {
+                                user.facebook.token = token;
+                                user.facebook.name = profile.name.givenName + ' ' + profile.name.familyName;
+                                user.facebook.email = profile.emails[0].value;
+
+                                user.save(function (err) {
+                                    if (err)
+                                        throw err;
+                                    return done(null, user);
+                                });
+                            }
                             return done(null, user); // user found, return that user
                         } else {
                             // if there is no user found with that facebook id, create them
@@ -185,19 +177,16 @@ module.exports = function (passport) {
         }));
 
     passport.use(new TwitterStrategy({
-
             consumerKey: configAuth.twitterAuth.consumerKey,
             consumerSecret: configAuth.twitterAuth.consumerSecret,
             callbackURL: configAuth.twitterAuth.callbackURL
-
         },
-        function (token, tokenSecret, profile, done) {
+        function (req, token, tokenSecret, profile, done) {
             // make the code asynchronous
             // User.findOne won't fire until we have all our data back from Twitter
             process.nextTick(function () {
 
                 if (!req.user) {
-
                     User.findOne({
                         'twitter.id': profile.id
                     }, function (err, user) {
@@ -209,6 +198,17 @@ module.exports = function (passport) {
 
                         // if the user is found then log them in
                         if (user) {
+                            if (!user.twitter.token) {
+                                user.twitter.token = token;
+                                user.twitter.name = profile.name.givenName + ' ' + profile.name.familyName;
+                                user.twitter.email = profile.emails[0].value;
+
+                                user.save(function (err) {
+                                    if (err)
+                                        throw err;
+                                    return done(null, user);
+                                });
+                            }
                             return done(null, user); // user found, return that user
                         } else {
                             // if there is no user, create them
@@ -249,13 +249,11 @@ module.exports = function (passport) {
         }));
 
     passport.use(new GoogleStrategy({
-
             clientID: configAuth.googleAuth.clientID,
             clientSecret: configAuth.googleAuth.clientSecret,
             callbackURL: configAuth.googleAuth.callbackURL,
-
         },
-        function (token, refreshToken, profile, done) {
+        function (req, token, refreshToken, profile, done) {
 
             // make the code asynchronous
             // User.findOne won't fire until we have all our data back from Google
@@ -271,7 +269,17 @@ module.exports = function (passport) {
                             return done(err);
 
                         if (user) {
+                            if (!user.google.token) {
+                                user.google.token = token;
+                                user.google.name = profile.name.givenName + ' ' + profile.name.familyName;
+                                user.google.email = profile.emails[0].value;
 
+                                user.save(function (err) {
+                                    if (err)
+                                        throw err;
+                                    return done(null, user);
+                                });
+                            }
                             // if a user is found, log them in
                             return done(null, user);
                         } else {
@@ -292,7 +300,6 @@ module.exports = function (passport) {
                             });
                         }
                     });
-
                 } else {
                     // user already exists and is logged in, we have to link accounts
                     var user = req.user; // pull the user out of the session
